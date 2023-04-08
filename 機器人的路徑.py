d1349e82b68910a295b74362ad77ee4975ff78a2
XxMilton <miltonblue0811@gmail.com>
2023-04-08 11:14:24 +0800

    Update 機器人的路徑.py
    :D

--- 機器人的路徑.py
+++ 機器人的路徑.py
@@ -2,66 +2,47 @@
 3 4
 1 3 5 4
 2 8 9 6
-10 15 12 13=88
+10 15 12 13=63
 '''
 n,m=map(int,input().split())
 A=[]#map
-AA=[[0]*n for i in range(m)]
+AA=[[0]*m for i in range(n)]
 P=[]#暫存路線
 p=0#總和
 min0=999
+nowi=0
+nowj=0
+lasti=0
+lastj=0
+v=[[-1,0],[0,1],[1,0],[0,-1]]
 for i in range(n):
     A.append(list(map(int,input().split())))
 for j in range(len(A)):
-    for i in range(len(A)[j]):
+    for i in range(len(A[j])):
         if A[j][i]<min0:
             min0=A[j][i]
-            P.append(min0)
-            AA[j][i]==True
-            if A[j+1][i]>A[j][i-1] and A[j-1][i]>A[j][i-1] and A[j][i+1]>A[j][i-1]:
-                if AA[j][i-1]==False:
-                    P.append(A[j][i-1])
-                    A[j][i-1]=A[j][i]
-                else:
-                    continue
-            elif A[j+1][i]>A[j][i+1] and A[j-1][i]>A[j][i+1] and A[j][i-1]>A[j][i+1]:
-                if AA[j][i+1]==False:
-                    P.append(A[j][i+1])
-                    A[j][i+1]=A[j][i]
-                else:
-                    continue
-            elif A[j+1][i]>A[j-1][i] and A[j][i+1]>A[j-1][i] and A[j][i-1]>A[j-1][i]:
-                if AA[j-1][i]==False:
-                    P.append(A[j-1][i])
-                    A[j-1][i]=A[j][i]
-                else:
-                    continue
-            elif A[j-1][i]>A[j+1][i] and A[j][i+1]>A[j+1][i] and A[j][i-1]>A[j+1][i]:
-                if AA[j+1][i]==False:
-                    P.append(A[j+1][i])
-                    A[j+1][i]=A[j][i]
-                else:
-                    continue
-for i in range(P):
-    p+=i
-print(p)
-'''
-for j in range(len(A)):
-    for i in range(len(A)[j]):
-        if A[j][i]<min0:
-            min0=A[j][i]
-            P.append(min0)
-            if A[j+1][i]>A[j][i-1] and A[j-1][i]>A[j][i-1] and A[j][i+1]>A[j][i-1]:
-                P.append(A[j][i-1])
-                A[j][i-1]=A[j][i]
-            elif A[j+1][i]>A[j][i+1] and A[j-1][i]>A[j][i+1] and A[j][i-1]>A[j][i+1]:
-                P.append(A[j][i+1])
-                A[j][i+1]=A[j][i]
-            elif A[j+1][i]>A[j-1][i] and A[j][i+1]>A[j-1][i] and A[j][i-1]>A[j-1][i]:
-                P.append(A[j-1][i])
-                A[j-1][i]=A[j][i]
-            elif A[j-1][i]>A[j+1][i] and A[j][i+1]>A[j+1][i] and A[j][i-1]>A[j+1][i]:
-                P.append(A[j+1][i])
-                A[j+1][i]=A[j][i]
-'''
-
+            nowi=i
+            nowj=j
+P.append(min0)
+AA[nowi][nowj]=True
+while True:
+    nexti=nowi#暫存下一個i
+    nextj=nowj#暫存下一個j
+    lasti=nowi
+    lastj=nowj
+    tempMin=999999
+    for k in v:
+        if(0<=nowi+k[0]<n)and(0<=nowj+k[1]<m):#處理要在邊界內
+            if(A[nowi+k[0]][nowj+k[1]]<tempMin) and AA[nowi+k[0]][nowj+k[1]]==False:
+                nexti=nowi+k[0]
+                nextj=nowj+k[1]
+                tempMin=A[nowi+k[0]][nowj+k[1]]
+    #print(nexti)
+    #print(nextj)
+    if nowi==nexti and nowj==nextj:
+        break
+    nowi=nexti
+    nowj=nextj
+    AA[nowi][nowj]=True
+    P.append(A[nowi][nowj])
+print(sum(P))
